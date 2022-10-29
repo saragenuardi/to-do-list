b389bf7be07f06d06bf1e89bbd590e545d03aac2
saragenuardi <saretta.summer16@gmail.com>
2022-10-29 19:08:40 +0200

    first commit

---
+++ src/ToDo.js
@@ -0,0 +1,11 @@
+ class ToDo {
+
+    constructor(id, name, description, done) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.done = done;
+    }
+}
+
+export default ToDo;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import List from './List';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <List />
   </React.StrictMode>
 );
 
